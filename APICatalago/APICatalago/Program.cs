d1b2637f4afe3399ad8ed8fbe5f3857b03270fc6
Wandersondu <wdutra@outlook.pt>
2022-08-24 23:33:54 +0100

    adicionando arquivos

--- APICatalago/APICatalago/Program.cs
+++ APICatalago/APICatalago/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ C#-Iniciante/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Olá {name}, Seja muito bem vindo!");
+Console.WriteLine("Digite o ano de nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year;
+Console.WriteLine($" O Sr. {name} tem a idade de: {age} anos!");
+
+if (age >= 18 ) 
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else 
+{
+    Console.WriteLine("você é menor de idade. ");
+}
+*/
+
+string[] names = {"Dutra"}
