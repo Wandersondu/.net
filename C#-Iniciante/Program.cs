340e954f198d385bb9be2f9b3ba59f59a236d3c2
Wandersondu <wdutra@outlook.pt>
2022-08-26 23:23:51 +0100

    atualizando curso c#

--- C#-Iniciante/Program.cs
+++ C#-Iniciante/Program.cs
@@ -16,4 +16,22 @@ else
 }
 */
 
-string[] names = {"Dutra"}
+class Program
+{
+    static void Main()
+    {
+        int i = 10;
+        int i2 = i;
+        i2 = 20;
+
+        Test t = new Test();
+        t.X = 12;
+        
+        Test t2 = t;
+        t2 = new Test();
+        t2.X = 20;
+
+        Console.WriteLine(t.X);
+    }
+}
+
